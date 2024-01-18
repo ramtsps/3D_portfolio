4ff3d588c8e1a4d20e7a06ee6c16b710fe18c8c0
ramtsps <116824915+ramtsps@users.noreply.github.com>
2024-01-18 09:40:40 +0530

    Update Works.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Tilt from "react-tilt";
+import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { github } from "../assets";
